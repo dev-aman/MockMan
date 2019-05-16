9bc36e74f9be8a69f7f77370fbe1ef30dc6ec366
Aman Gupta <aman.gupta@shuttl.com>
2019-05-17 04:36:40 +0530

    Add logs to tell the user to change the path to MockMan again in terminal

--- MockMan/rollback.py
+++ MockMan/rollback.py
@@ -7,6 +7,9 @@ def rollback():
         shutil.rmtree("../../mock-man/MockMan")
         shutil.move("../mock-man-backup/MockMan", "../../mock-man")
         shutil.rmtree("../mock-man-backup")
+        print("As we have removed the MockMan Directory and replaced with the last backup files. Please change the path to MockMan.")
+        print("run cd ..")
+        print("run cd MockMan")
     except Exception as e:
         print(e)
 
